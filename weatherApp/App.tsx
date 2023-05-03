dbe67cf1bde4aa74a6e08521374a9c5000bfa98c
Gergely Laszlo Csepanyi <gergely-laszlo.csepanyi@nixs.com>
2023-05-03 11:14:11 +0200

    Start working on the Tab navigation.

--- weatherApp/App.tsx
+++ weatherApp/App.tsx
@@ -2,12 +2,32 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {API_GEODB_CITIES_TOKEN} from '@env';
 import MainScreen from './src/screens/MainScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+export type RootTabParamList = {
+  MainScreen: undefined;
+  FavouritesScreen: undefined;
+};
 
 function App(): JSX.Element {
+  const Tab = createBottomTabNavigator<RootTabParamList>();
+
   return (
-    <SafeAreaView>
-      <MainScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Screen
+          name="MainScreen"
+          component={MainScreen}
+          options={{title: 'Location'}}
+        />
+        <Tab.Screen
+          name="FavouritesScreen"
+          component={MainScreen}
+          options={{title: 'Favourites'}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
