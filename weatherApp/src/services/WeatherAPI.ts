81aae24c0cb700221543f4b5bf7704ef0673702b
Gergely Laszlo Csepanyi <gergely-laszlo.csepanyi@nixs.com>
2023-05-09 18:03:07 +0200

    Start working on the forecast.

--- weatherApp/src/services/WeatherAPI.ts
+++ weatherApp/src/services/WeatherAPI.ts
@@ -60,6 +60,60 @@ export type WeatherResponse = {
   };
 };
 
+type WeatherForecastWeatherType = {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  temp: {
+    day: number;
+    min: number;
+    max: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  feels_like: {
+    day: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  pressure: number;
+  humidity: number;
+  weather: [
+    {
+      id: number;
+      main: 'Rain';
+      description: 'light rain';
+      icon: '10d';
+    },
+  ];
+  speed: 2.7;
+  deg: 209;
+  gust: 3.58;
+  clouds: 53;
+  pop: 0.7;
+  rain: 2.51;
+};
+
+export type WeatherForecastResponse = {
+  city: {
+    id: 3163858;
+    name: 'Zocca';
+    coord: {
+      lon: 10.99;
+      lat: 44.34;
+    };
+    country: 'IT';
+    population: 4593;
+    timezone: 7200;
+  };
+  cod: '200';
+  message: 0.0582563;
+  cnt: 7;
+  list: [];
+};
+
 export interface WeatherApiInterface {
   fetchWeather: (
     longitude: number,
@@ -80,7 +134,6 @@ class WeatherApi implements WeatherApiInterface {
     const options = {
       method: 'GET',
     };
-    // console.log(url);
     return request<WeatherResponse>(APIType.weatherAPI, url, options);
   };
 }
